ed98462d5a94d61d079d68e17e5327872dd898fc
RosenTodorov <kobe_8@abv.bg>
2014-09-04 13:30:48 +0300

    new changes

--- InfernoDb.Models/Sale.cs
+++ InfernoDb.Models/Sale.cs
@@ -2,16 +2,29 @@
 {
     using System.ComponentModel.DataAnnotations;
     using System;
+    using MongoDB.Bson.Serialization.Attributes;
 
-   public class Sale
+   public class Sale : MongoDbID
     {
        [Key]
         public int SaleId { get; set; }
         
+       [NonSerialized]
        public int ProductId { get; set; }
 
        public decimal UnitPrice { get; set; }
 
-       public DateTime? Date { get; set; }
+       public decimal  Profit { get; set; }
+
+       public DateTime? DateSales { get; set; }
+
+       [BsonIgnore]
+       public virtual Product ProduckId { get; set; }
+
+       [BsonIgnore]
+       public virtual Region Regions { get; set; }
+
+      
+
     }
 }
