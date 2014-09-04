d8d3c67f06ea2533151bd70f8ef891d40f655d40
RosenTodorov <kobe_8@abv.bg>
2014-09-04 13:38:14 +0300

    new changes

--- InfernoDb.Client/InfernoDbMain.cs
+++ InfernoDb.Client/InfernoDbMain.cs
@@ -14,18 +14,30 @@
     using System.Data.OleDb;
     using System.Linq;
     using InfernoDb.Data;
+    using System.Xml.Linq;
+    using XML;
 
     public class InfernoDbMain
     {
 
         public static void Main()
         {
+             UowData data = new UowData();
+
+        var findSales = data.Sales.All();
+        foreach (var sale in findSales)
+        {
+          
+            XmlExport report = new XmlExport();
 
 
-            using (DataContext db = new DataContext())
-            {
-               
-            }
+            Console.WriteLine((DateTime)sale.Date);
+
+            report.Export((DateTime)sale.Date, sale.ProductId.ToString(), (double)sale.UnitPrice);
+        
+        }
+
+        
 
         }
     }
